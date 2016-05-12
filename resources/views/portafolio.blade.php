8b15df84390f1a933c1ec084e5f9992dc8e83f76
tananta21 <terre.21az@gmail.com>
2016-05-12 10:29:52 -0500

    mi portafolio web

---
+++ resources/views/portafolio.blade.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>blog final</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/query.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+    <!--<link rel="stylesheet" href="css/bootstrap.css">-->
+</head>
+<body style="font-family: lato;">
+<div class="container-fluid" style="padding: 0rem">
+    <header  class="container_dad">
+        <div class="container_son">
+            <div class="mi_nav">
+                <nav id="menu" >
+                    <a href="#" class="nav-mobile" id="nav-mobile"></a>
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Sobre mi</a></li>
+                        <li><a href="#">Encoder</a></li>
+                        <li><a href="#">musica</a></li>
+                        <li><a href="#">contacto</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <div class="header_fondo">
+            <div class="container_fondo">
+                <div class="foto_header">
+                    <figure class="figure_header" >
+                        <img class="figure_img"src="img/perfil/perfil2.jpg" alt="">
+                    </figure>
+                </div>
+                <h2 class="header_fondo_name">kevin anthony tananta del aguila</h2>
+                <p class="header-fondo-description">
+                    Hola! Soy Kevin , en esta portafolio web encontraras <br> informacion
+                    sobre mi vida laboral, académica y personal, asi que
+                    <br><strong style="font-size: 1.6em;">!Te invito a conocerme¡</strong>
+                </p>
+            </div>
+            <div class="unirme">
+                <h2>Mis Redes sociales</h2>
+                <a class="redes_sociales" href=""><i class="fa fa-facebook "></i></a>
+                <a class="redes_sociales" href=""><i class="fa fa-twitter"></i></a>
+                <a class="redes_sociales" href=""><i class="fa fa-youtube "></i></a>
+                <a class="redes_sociales" href=""><i class="fa fa-snapchat "></i></a>
+                <a class="redes_sociales" href=""><i class="fa fa-google-plus "></i></a>
+                <a class="redes_sociales" href=""><i class="fa fa-instagram "></i></a>
+                <a class="redes_sociales" href=""><i class="fa fa-github "></i></a>
+                <a class="redes_sociales" href=""><i class="fa fa-linkedin "></i></a>
+
+            </div>
+        </div>
+    </header>
+
+    <section class="section1">
+        <div class="section1_name">
+            <h2 style="font-size: 2.2em;color: rgb(102, 102, 102); font-weight: bold">SOBRE MI</h2>
+            <h3 style="color: #0f0f0f; font-family: 'Arial Black'">Desarrollador y Programador Web, Futuro Ingeniero de Sistemas Especialista en Manejo sobre Tecnologias de la informacion</h3>
+        </div>
+
+        <div class="section1_second">
+            <div class="section1_figure">
+                <figure class="figure_section1">
+                    <img class="section1_perfil" src="img/perfil/perfil5_opt.jpg" alt="perfil">
+                </figure>
+                <h3 style="text-align: center; font-weight: bold;color: #1b6d85"><i class="fa fa-quote-left" aria-hidden="true"></i> La formación y/o el talento hacen poco sin pasión <i class="fa fa-quote-right" aria-hidden="true"></i></h3>
+            </div>
+
+            <div class="section1_second_2">
+                <p class="section1_text">
+                    Mi nombre es Kevin Anthony Tananta Del Aguila, actualmente soy estudiante en Ingeniería de Sistemas e
+                    Informacion en la Universidad Cientifica Del Peru  y Practicante en Lenguajes y Sistemas Informáticos: Especialidad en Tecnologías del Lenguaje en la Web.
+                    <br>Me gusta aprender las últimas tecnologías web y <strong>compartir conocimiento</strong> con la comunidad.
+                </p>
+            </div>
+        </div>
+        <div class="conocimientos">
+            <h2 class="lang">Conocimientos de los Principales Lenguajes de Programacion</h2>
+            <div class="section1_second lenguajes">
+                <div class="type_lang" style="border-radius:5%; padding: 2rem; background: mediumpurple">
+                    <h3>PHP</h3>
+                </div>
+                <div class="type_lang" style="border-radius:5%; padding: 2rem; background: palevioletred ">
+                    <h3>JAVA</h3>
+                </div>
+                <div class="type_lang" style="border-radius:5%; padding: 2rem; background: greenyellow">
+                    <h3>PHYTON</h3>
+                </div>
+
+                <div class="type_lang" style="border-radius:5%; padding: 2rem; background: darkseagreen">
+                    <h3>NODE JS</h3>
+                </div>
+
+            </div>
+        </div>
+        <div class="conocimientos">
+            <h2 class="lang">Y en los Principales Manejadores de base de datos</h2>
+            <div class="section1_second gestores">
+                <div class="type_ges" style="border-radius:5%; padding: 2rem; background: coral">
+                    <h3>MYSQL</h3>
+                </div>
+                <div class="type_ges" style="border-radius:5%; padding: 2rem; background: silver ">
+                    <h3>SQL SERVER</h3>
+                </div>
+                <div class="type_ges" style="border-radius:5%; padding: 2rem; background: #336791">
+                    <h3>POSTGRE SQL</h3>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="section2">
+        <h2 style="color: #2aabd2; text-align: center; width: 90%;">CONOCE MI EMPRESA <strong>ENCODER XXI</strong></h2>
+        <div class="section2_ul">
+            <div class="brujula">
+                <figure><img src="img/encoder_xxi.png" alt=""></figure>
+            </div>
+            <div class="section2_list">
+                <p class="section2_description">
+                    <strong>Encoder XXI </strong> es una empresa dedicada al desarrollo de software de aplicación para la administración de empresas.
+                    El objetivo de  <strong>Encoder XXI </strong> es incrementar la competitividad de nuestros clientes, ofreciéndoles productos y servicios adecuados y
+                    adaptados a su sistema de gestión, que les permita diferenciarse de la  competencia.
+                    Nuestro compromiso es el de procurar la máxima satisfacción a nuestros clientes, a través de nuestros productos y servicios, atendiendo el conjunto de sus necesidades.
+                </p>
+            </div>
+        </div>
+
+        <div class="section2_text">
+            <h2 style="color: #2aabd2; text-align: center;" >Nuestros Servicios</h2>
+        </div>
+    </section>
+
+    <section class="section3">
+        <div class="section3_direction">
+            <i class="fa fa-user fa-5x"></i>
+            <p class="descrption_services">Lorem ipsum dolor sit amet, consectetur <br>
+                adipisicing elit. Animi, optio?</p>
+            <div class="click">
+                <h3 class="vermas" >DESARROLLO WEB</h3>
+            </div>
+        </div>
+        <div class="section3_direction">
+            <i class="fa fa-laptop fa-5x"></i>
+            <p class="descrption_services">Lorem ipsum dolor sit amet, consectetur <br>
+                adipisicing elit. Animi, optio?</p>
+            <div class="click">
+                <h3 class="vermas" >DESARROLLO SOFTWARE</h3>
+            </div>
+        </div>
+        <div class="section3_direction">
+            <i class="fa fa-paint-brush fa-5x"></i>
+            <p class="descrption_services">Lorem ipsum dolor sit amet, consectetur <br>
+                adipisicing elit. Animi, optio?</p>
+            <div class="click">
+                <h3 class="vermas" >DISEÑO GRAFICO</h3>
+            </div>
+        </div>
+        <div class="section3_direction">
+            <i class="fa fa-phone fa-5x"></i>
+            <p class="descrption_services">Lorem ipsum dolor sit amet, consectetur <br>
+                adipisicing elit. Animi, optio?</p>
+            <div class="click">
+                <h3 class="vermas" >AUDITORIA</h3>
+            </div>
+        </div>
+
+
+    </section>
+
+    <section class="section4">
+        <h2 class="section4_name" >ESTOS SON ALGUNOS DE MIS PROYECTOS</h2>
+        <h3 class="section4_text" >Sitios Webs Y Aplicaciones</h3>
+        <div class="section4_bloque1">
+            <figure class="figure_section4" ><img class="section4_img" src="img/fotos/im1.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/fotos/im2.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/fotos/im3.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/fotos/im4.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/fotos/im5.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/fotos/im6.jpg" alt=""></figure>
+
+        </div>
+    </section>
+
+    <section class="section4">
+        <h2 class="section4_name" >CONOCE MAS SOBRE MI</h2>
+        <h3 class="section4_text" >MIS ESTUDIOS</h3>
+        <div class="section4_bloque1">
+            <figure class="figure_section4" ><img class="section4_img" src="img/fotos/nor.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/fotos/ucp.jpeg" alt=""></figure>
+
+        </div>
+        <h3 class="section4_text" >PERSONAS IMPORTANTES</h3>
+        <div class="section4_bloque1">
+            <figure class="figure_section4" ><img class="section4_img" src="img/hobbies/foto6.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/hobbies/foto2.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/hobbies/foto3.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/hobbies/foto4.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/hobbies/foto8.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/hobbies/foto7.jpg" alt=""></figure>
+        </div>
+        <h3 class="section4_text" >MIS HOBBIES</h3>
+        <div class="section4_bloque1">
+            <figure class="figure_section4" ><img class="section4_img" src="img/personas/hobbie1.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/personas/hobbie2.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/personas/hobbie3.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/personas/hobbie5.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/personas/hobbie6.jpg" alt=""></figure>
+            <figure class="figure_section4" ><img class="section4_img" src="img/personas/hobbie7.jpg" alt=""></figure>
+        </div>
+    </section>
+
+
+    <footer >
+        <div class=footer>
+            <div class="footer_bloque1" >
+                <div class="maps">
+                    <h3>Recuerda que me puedes encontrar en <strong>Jr. San Martin N°148 - Tarapoto</strong> </h3>
+                    <figure>
+                        <a target="_blank" href="https://www.google.com.pe/maps/@-6.4862569,-76.3633308,15.75z"><img src="img/maps.png" alt="" style="width: 100%"></a>
+                    </figure>
+                </div>
+            </div>
+
+            <div class="bloque2" >
+                <h3>Contacte commigo</h3>
+                <form class="section6_form">
+                    <input class="form_input" type="text" placeholder="Tu nombre" >
+                    <input class="form_input" type="email" placeholder="Tu email"  >
+                    <textarea class="form_input"></textarea>
+                    <input class="form_submit" type="submit" value="enviar" >
+                </form>
+                <h3>No olvides mis Redes sociales</h3>
+                <a class="redes_sociales_fo" href=""><i class="fa fa-facebook "></i></a>
+                <a class="redes_sociales_fo" href=""><i class="fa fa-twitter"></i></a>
+                <a class="redes_sociales_fo" href=""><i class="fa fa-youtube "></i></a>
+                <a class="redes_sociales_fo" href=""><i class="fa fa-snapchat "></i></a>
+                <a class="redes_sociales_fo" href=""><i class="fa fa-google-plus "></i></a>
+                <a class="redes_sociales_fo" href=""><i class="fa fa-instagram "></i></a>
+                <a class="redes_sociales_fo" href=""><i class="fa fa-github "></i></a>
+                <a class="redes_sociales_fo" href=""><i class="fa fa-linkedin "></i></a>
+                <div style="padding-top: 2rem;">Copyright © 2016-2018 Kevin Anthony Tananta Del Aguila.</div>
+            </div>
+        </div>
+    </footer>
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+
+<script>
+    $(function() {
+
+        var btn_movil = $('#nav-mobile'),
+                menu = $('#menu').find('ul');
+
+        // Al dar click agregar/quitar clases que permiten el despliegue del menú
+        btn_movil.on('click', function (e) {
+            e.preventDefault();
+
+            var el = $(this);
+
+            el.toggleClass('nav-active');
+            menu.toggleClass('open-menu');
+        })
+    });
+</script>
+
+
+</body>
+</html>
