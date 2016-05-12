b96c4b6c19ace84abb6ca25863f7b10b6396c0d0
tananta21 <terre.21az@gmail.com>
2016-05-12 16:40:54 -0500

    modified index conoceme

--- resources/views/portafolio.blade.php
+++ resources/views/portafolio.blade.php
@@ -24,7 +24,7 @@
                         <li><a href="#">Inicio</a></li>
                         <li><a href="#">Sobre mi</a></li>
                         <li><a href="#">Encoder</a></li>
-                        <li><a href="#">musica</a></li>
+                        <li><a href="#">conoceme</a></li>
                         <li><a href="#">contacto</a></li>
                     </ul>
                 </nav>
